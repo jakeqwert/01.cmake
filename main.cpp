c2a40d699640db63c6b4f7757d1174672f274732
Jake <eliseevz@mail.ru>
2022-06-08 23:25:09 +0300

    docker-compose.yml

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <spdlog/spdlog.h> // для локальной сборки sudo apt install libspdlog-dev
                            // travis CI подготовит образ с пакетом нужным
 
+// git add docker-compose.yml
+// git commit -m "add docker-compose.yml"
+// git push -u origin master
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console"); //пишем в console
