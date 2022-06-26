b9db230581f01a7d7c9bceb46695373d67fa6a01
Jake <eliseevz@mail.ru>
2022-06-26 10:19:41 +0300

    refactoring CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -3,9 +3,26 @@
 #include <spdlog/spdlog.h> // для локальной сборки sudo apt install libspdlog-dev
                            // travis CI подготовит образ с пакетом нужным
 
-// git add docker-compose.yml
-// git commit -m "add docker-compose.yml"
-// git push -u origin master
+//
+
+// git status
+// git add .
+// git commit -m "edit CMakeLists.txt, .github/workflows/pipeline.yml for GitHubAction"
+// git push origin
+// git push origin_loc
+// git push origin_rg
+
+//$ git remote -v
+//origin  git@github.com:jakeqwert/01.cmake.git (fetch)
+//origin  git@github.com:jakeqwert/01.cmake.git (push)
+//origin_loc      git@gitlab:root/01.cmake.git (fetch)
+//origin_loc      git@gitlab:root/01.cmake.git (push)
+//origin_rg       http://h034-ubu-01/jake/01.cmake.git (fetch)
+//origin_rg       http://h034-ubu-01/jake/01.cmake.git (push)
+
+// Запуск                   git@gitlab:root/01.cmake.git
+// docker-compose up -d     Create and start containers /запустите проект в фоновом режиме
+
 int main()
 {
     auto logger = spdlog::stdout_logger_mt("console"); //пишем в console
--- test_version.cpp
+++ test_version.cpp
@@ -1,16 +1,15 @@
-#define BOOST_TEST_MODULE helloworld_test_module // файл только тесты без main(), сделает исполняемым
-//           тест модуль  helloworld_test_module
-
+#define BOOST_TEST_MODULE test_version_test_module // файл только тесты без main(), сделает исполняемым
+//           тест модуль  test_version_test_module
 #include "lib.h"
 
 #include <boost/test/unit_test.hpp>  // boost unit_test
 
-//+++ название теста helloworld_test_suite
-BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
+//+++ название теста project_01_cmake_test_suite
+BOOST_AUTO_TEST_SUITE(project_01_cmake_test_suite)
 // BOOST_AUTO_TEST_SUITE - макрос, вставит код cpp
 
-// отдельный тест CASE, с именем helloworld_test_version
-BOOST_AUTO_TEST_CASE(helloworld_test_version)
+// отдельный тест CASE, с именем test_valid_version
+BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0); // код выполняет тестирование
 }
